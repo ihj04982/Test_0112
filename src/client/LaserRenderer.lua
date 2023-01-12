73307780193f8d49c05c930c67a58c979705602a
dmn_nmd <ihj04982@gmail.com>
2023-01-12 17:45:00 +0900

    230112

--- src/client/LaserRenderer.lua
+++ src/client/LaserRenderer.lua
@@ -8,10 +8,10 @@ function LaserRenderer.createLaser(startPos, endPos)
     laserPart.CanCollide = false
     laserPart.Color = Color3.fromRGB(225, 0, 0)
     laserPart.Material = Enum.Material.Neon
-    laserPart.Parent = Workspace
+    laserPart.Parent = workspace
 
     local laserDistance = (endPos - startPos).Magnitude
-    lasePart.Size = Vector3.new(0.2, 0.2, laserDistance)
+    laserPart.Size = Vector3.new(0.2, 0.2, laserDistance)
     local laserCFrameTemp = CFrame.lookAt(startPos, endPos)
     laserPart.CFrame = laserCFrameTemp * CFrame.new(0,0,-laserDistance*0.5)
 end
